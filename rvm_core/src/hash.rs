e15d5c0530dd1040e2f99f6d52c055cba6257a1d
GodaHaruki <102050420+GodaHaruki@users.noreply.github.com>
2023-04-27 12:14:03 +0900

    fix error

--- rvm_core/src/hash.rs
+++ rvm_core/src/hash.rs
@@ -4,9 +4,10 @@ pub struct Hash(String);
 
 impl Hash {
     pub fn new(input: &str) -> Self{
-        let mut sha256 = Sha256::new();
-        sha256.input_str(input);
+        // let mut sha256 = Sha256::new();
+        // sha256.input_str(input);
 
-        sha256.result_str()
+        // sha256.result_str()
+        unimplemented!()
     }
 }
