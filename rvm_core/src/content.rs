5ae0abec8d788f8d837c0540de43a8d203945232
Okay-Love <haru.11.saki.11@gmail.com>
2023-04-27 23:59:13 +0900

    implemented new

--- rvm_core/src/content.rs
+++ rvm_core/src/content.rs
@@ -1,10 +1,20 @@
 use crate::blob::Blob;
 use crate::tree::Tree;
-use crate::path::Path;
+use crate::hash::Hash;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Content {
     Tree(Tree),
     Blob(Blob),
+}
+
+impl Content {
+    pub fn get_hash(&self) -> Hash {
+        use self::Content::*;
+        match self {
+            Tree(T) => T.hash.clone(),
+            Blob(B) => B.hash.clone(),
+        }
+    }
 }
--- rvm_core/src/tree.rs
+++ rvm_core/src/tree.rs
@@ -10,9 +10,16 @@ pub struct Tree {
 }
 
 impl Tree {
+    pub fn gen_contents_hash(&self) -> String{
+        let mut contents_hashes = String::new();
+        self.contents.iter().for_each(
+            |c| contents_hashes += &c.get_hash().to_string());
+        return contents_hashes;
+    }
+
     pub fn new(name: String, contents: Vec<Content>) -> Self{
         Self{
-            hash: Hash::new(&(name + format!("{#?}", Vec<Content>))),
+            hash: Hash::new(&name),
             name,
             contents,
         }
